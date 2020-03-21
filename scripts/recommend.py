07a0cda7e4136717b9c5f01960d5a39ac11f6ecf
David <2319712368@qq.com>
2020-03-22 00:29:45 +0800

    Fix: bug from set function

--- scripts/recommend.py
+++ scripts/recommend.py
@@ -4,8 +4,8 @@ import redis
 
 
 if __name__ == '__main__':
-    os.environ["PYSPARK_PYTHON"] = "/Users/david/.pyenv/versions/3.5.2/bin/python"
-    os.environ["PYSPARK_DRIVER_PYTHON"] = "/Users/david/.pyenv/versions/3.5.2/bin/python"
+    os.environ["PYSPARK_PYTHON"] = "/usr/bin/python3"
+    os.environ["PYSPARK_DRIVER_PYTHON"] = "/usr/bin/python3"
     conn = redis.Redis(host='127.0.0.1', port=6379, db=0);
     sc = SparkContext()
 
@@ -13,12 +13,10 @@ if __name__ == '__main__':
 
     userlist = base.map(lambda line: (line[0][0], line[0][1]))
     movielist = base.map(lambda line: (line[0][1], line[0][0]))
-    #
     movie_common = movielist.join(movielist).map(lambda line: ((line[1][0], line[1][1]), 1)).reduceByKey(lambda a, b: a+b).map(lambda line: (line[0][0], line[0][1]))
 
-    recommend_list = movie_common.join(userlist).map(lambda line: (line[1][0], line[1][1])).reduceByKey(lambda a, b: [a]+[b] if type(a) != list and type(b) != list else (a+b if type(a) == list and type(b) == list else (a+[b] if type(a) == list else [a]+b))).map(lambda line: (line[0], set(line[1])))
+    recommend_list = movie_common.join(userlist).map(lambda line: (line[1][0], line[1][1])).reduceByKey(lambda a, b: [a]+[b] if type(a) != list and type(b) != list else (a+b if type(a) == list and type(b) == list else (a+[b] if type(a) == list else [a]+b))).map(lambda line: (line[0], line[1]))
     # result stores in redis
-
     for item in recommend_list.toLocalIterator():
         conn.hset("recommends", item[0][0], item[0][1])
 
